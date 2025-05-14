b6649afc1c3f62dd0d7ed334027974961d976a34
Buggg43 <kacperwerra12@gmail.com>
2025-05-14 12:45:53 +0200

    Add project files.

---
+++ HelloBank/BankAccount.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloBank
+{
+    public class BankAccount
+    {
+        private static int s_accountNumberSeed = 1234567890;
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance 
+        { get
+            {
+                decimal balance =0;
+                foreach (var item in _allTransactions)
+                {
+                    balance += item.Amount;
+                }
+                return balance;
+            }
+        }
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+            Number = s_accountNumberSeed.ToString();
+            s_accountNumberSeed++;
+
+            Owner = name;
+            MakeDeposit(initialBalance, DateTime.Now, "Initial balance");
+        }
+
+        private List<Transactions> _allTransactions = new List<Transactions>();
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if (amount <=0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "The deposit amount must be positive.");
+            }
+            var deposit = new Transactions(amount, date, note);
+            _allTransactions.Add(deposit);
+        }
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+            if (amount > Balance)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal.");
+            }
+            var withdrawal = new Transactions(amount, date, note);
+            _allTransactions.Add(withdrawal);
+        }
+        public string GetAccountHistory()
+        {
+            var report = new System.Text.StringBuilder();
+
+            decimal balance = 0;
+            report.AppendLine("Date\t\tAmount\tBalance\tNote");
+            foreach (var item in _allTransactions)
+            {
+                balance += item.Amount;
+                report.AppendLine($"{item.Date.ToShortDateString()}\t{item.Amount}\t{balance}\t{item.Notes}");
+            }
+
+            return report.ToString();
+        }
+
+    }
+}
---
+++ HelloBank/Transactions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloBank
+{
+    public class Transactions
+    {
+        public decimal Amount { get; }
+        public DateTime Date { get; }
+        public string Notes { get; }
+
+        public Transactions(decimal amount, DateTime date, string notes)
+        {
+            Amount = amount;
+            Date = date;
+            Notes = notes;
+        }
+    }
+}
